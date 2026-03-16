a7ed3f0f91edd057507919bae6d5494e94222bd2
MarcoMedeiros99 <marcomed07@gmail.com>
2026-03-16 13:02:39 -0300

    Project created

---
+++ src/test/java/com/marcoMedeiros/MyAnimeList/MyAnimeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoMedeiros.MyAnimeList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAnimeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
