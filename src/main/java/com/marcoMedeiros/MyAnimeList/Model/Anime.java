856556f11daa6ffd52e4326d5f5fdd82972cdbc2
MarcoMedeiros99 <marcomed07@gmail.com>
2026-03-17 17:55:20 -0300

    Created Model

---
+++ src/main/java/com/marcoMedeiros/MyAnimeList/Model/Anime.java
@@ -0,0 +1,108 @@
+package com.marcoMedeiros.MyAnimeList.Model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "tb_anime")
+public class Anime implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    //Mapping & Attributes
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    private String titulo;
+    private String genero;
+    private String anoLancamento;
+    private Double notaPessoal;
+    private Boolean assistido;
+
+    //Constructors
+
+
+    public Anime() {
+    }
+
+    public Anime(Long id, String titulo, String genero, String anoLancamento, Double notaPessoal, Boolean assistido) {
+        this.id = id;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+        this.notaPessoal = notaPessoal;
+        this.assistido = assistido;
+    }
+
+    //Getter and Setter
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(String anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public Double getNotaPessoal() {
+        return notaPessoal;
+    }
+
+    public void setNotaPessoal(Double notaPessoal) {
+        this.notaPessoal = notaPessoal;
+    }
+
+    public Boolean getAssistido() {
+        return assistido;
+    }
+
+    public void setAssistido(Boolean assistido) {
+        this.assistido = assistido;
+    }
+
+    //Equals() and HashCode()
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Anime anime = (Anime) o;
+        return Objects.equals(id, anime.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
